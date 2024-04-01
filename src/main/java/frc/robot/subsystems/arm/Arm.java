b5b843ae2a918bcd8f0830073574e6e3f613e7e0
tzhang <xiaogang.tony.zhang@gmail.com>
2024-04-01 00:45:11 -0400

    replace arm encoder

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -22,7 +22,7 @@ public class Arm extends SubsystemBase {
   public static final int ARM_LEVEL_STATION_INTAKE = 4;
   public static final int ARM_LEVEL_HANG = 5;
 
-  private static final double INITIAL_ARM_RADS = -0.879; // -29
+  private static final double INITIAL_ARM_RADS = -0.412; // -29
   // public static final double INITIAL_ARM_RADS = -0.316; // -22 degree
   public static final double ARM_DEGREES[] = {
     INITIAL_ARM_RADS, // STOW
