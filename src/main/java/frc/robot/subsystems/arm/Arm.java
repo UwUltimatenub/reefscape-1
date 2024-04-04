66dd9627992a7d8e233ad8717a0f8f28f800e8f7
Olivia Zhong <olivia.zhong@cautiontape.ca>
2024-04-04 14:56:43 -0400

    fix offset

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -22,7 +22,7 @@ public class Arm extends SubsystemBase {
   public static final int ARM_LEVEL_STATION_INTAKE = 4;
   public static final int ARM_LEVEL_HANG = 5;
 
-  private static final double INITIAL_ARM_RADS = -0.395; // -29
+  private static final double INITIAL_ARM_RADS = -0.352; // -29
   // public static final double INITIAL_ARM_RADS = -0.316; // -22 degree
   public static final double ARM_DEGREES[] = {
     INITIAL_ARM_RADS, // STOW
